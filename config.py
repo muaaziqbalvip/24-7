7442eab65f3d1c5c3e1a4c19aa6d36b513e6ddfe
muaaziqbalvip <muaaziqbal.vip@gmail.com>
2026-04-11 12:16:04 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI = os.getenv("GEMINI_API_KEY")
+GROQ = os.getenv("GROQ_API_KEY")
+OPENROUTER = os.getenv("OPENROUTER_API_KEY")
+
+CHANNEL_ID = "@your_channel"
